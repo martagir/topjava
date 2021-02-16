edea30d7cb3c986ded0b9e14df0c483fafa71380
tagirovmf <Tmf20201812>
2021-02-16 11:42:37 +0300

    HW2 created

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -16,7 +16,7 @@ public class User extends AbstractNamedEntity {
 
     private Date registered = new Date();
 
-    private Set<Role> roles;
+    private Set<Role>  roles;
 
     private int caloriesPerDay = DEFAULT_CALORIES_PER_DAY;
 
