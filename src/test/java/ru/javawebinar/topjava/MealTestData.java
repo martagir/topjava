a89c686d99c58a32377d34127f7242a5964be79c
tagirovmf <Tmf20201812>
2021-02-26 16:58:25 +0300

    HW-3-opt fixed spring, started tests

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,44 @@
+package ru.javawebinar.topjava;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTestData {
+    public static final int MEAL_1_ID = START_SEQ + 2;
+    public static final int MEAL_2_ID = START_SEQ + 3;
+    public static final int MEAL_3_ID = START_SEQ + 4;
+    public static final int MEAL_4_ID = START_SEQ + 5;
+    public static final int MEAL_5_ID = START_SEQ + 6;
+    public static final int MEAL_6_ID = START_SEQ + 7;
+    
+    public static final Meal meal_1 = new Meal(MEAL_1_ID, null, "Завтрак", 250);
+    public static final Meal meal_2 = new Meal(MEAL_2_ID, null, "Обед", 250);
+    public static final Meal meal_3 = new Meal(MEAL_3_ID, null, "Ужин", 250);
+    public static final Meal meal_4 = new Meal(MEAL_4_ID, null, "Завтрак", 250);
+    public static final Meal meal_5 = new Meal(MEAL_5_ID, null, "Обед", 250);
+    public static final Meal meal_6 = new Meal(MEAL_6_ID, null, "Ужин", 250);
+
+    public static Meal getNew() {
+        return new Meal(null, LocalDateTime.now(), "Ночной дожор",  1500);
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).usingRecursiveComparison().ignoringFields("dateTime", "userId").isEqualTo(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("dateTime", "userId").isEqualTo(expected);
+    }
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,66 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Meal;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    static {
+        // Only for postgres driver logging
+        // It uses java.util.logging and logged via jul-to-slf4j bridge
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Test
+    public void create() {
+        Meal created = service.create(getNew(), USER_ID);
+        Integer newId = created.getId();
+        Meal newMeal = getNew();
+        newMeal.setId(newId);
+        assertMatch(created, newMeal);
+        assertMatch(service.get(newId, USER_ID), newMeal);
+    }
+
+    @Test
+    public void get() {
+        Meal meal_1 = service.get(MEAL_1_ID, USER_ID);
+        assertMatch(meal_1, MealTestData.meal_1);
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+}
