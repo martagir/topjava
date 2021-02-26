49132c056bb3a859cbc9ec60fa67e8186ce2039b
tagirovmf <Tmf20201812>
2021-02-26 16:05:06 +0300

    HW-3 impl

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2021-02-25 09:17:40.523000', 'Завтрак', 300, 100000),
+       ('2021-02-26 12:17:40.523000', 'Обед',    250, 100000),
+       ('2021-02-26 18:17:40.523000', 'Ужин',    250, 100000),
+       ('2021-02-26 09:17:40.523000', 'Завтрак', 250, 100001),
+       ('2021-02-26 12:17:40.523000', 'Обед',    250, 100001),
+       ('2021-02-26 18:17:40.523000', 'Ужин',    250, 100001);
