6a21d6f05b7143c4d02ab94865e51a36da072af1
tagirovmf <Tmf20201812>
2021-03-09 14:55:03 +0300

    patch 5-2 applied

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -41,11 +41,12 @@ public class JdbcUserRepository implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
-        } else if (namedParameterJdbcTemplate.update(
-                "UPDATE users SET name=:name, email=:email, password=:password, " +
-                        "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource) == 0) {
-            return null;
-        }
+        } else if (namedParameterJdbcTemplate.update("""
+                UPDATE users SET name=:name, email=:email, password=:password,
+                registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id
+        """, parameterSource) == 0) {
+        return null;
+    }
         return user;
     }
 
