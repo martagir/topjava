989cb52575aec35b47e8f4fbeb24eb48bb58d24a
tagirovmf <Tmf20201812>
2021-04-08 13:11:45 +0300

    HW-8 branch created

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -41,6 +41,7 @@ public class UserService {
         return checkNotFound(repository.getByEmail(email), "email=" + email);
     }
 
+
     @Cacheable("users")
     public List<User> getAll() {
         return repository.getAll();
